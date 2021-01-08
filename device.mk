5e62b6822e54e8e7ae64a0c6a3acf79a76a906cc
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-01-08 23:21:20 +0530

    Add Screen Dimentions

--- device.mk
+++ device.mk
@@ -1,4 +1,6 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+$(call inherit-product, vendor/samsung/gta3xlwifi/gta3xlwifi-vendor.mk)
+
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7904-common/universal7904-common.mk)
--- lineage_gta3xlwifi.mk
+++ lineage_gta3xlwifi.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/gta3xlwifi/full_gta3xlwifi.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
 PRODUCT_NAME := lineage_gta3xlwifi
 
 -include vendor/samsung/gta3xlwifi/BoardConfigVendor.mk
