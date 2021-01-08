8d1a82cee0a2adc8ba5d166cdff9daaf8568addf
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-01-08 21:38:20 +0530

    Add lunch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gta3xlwifi.mk
+
+COMMON_LUNCH_CHOICES := \
+     lineage_gta3xlwifi-userdebug \
+     lineage_gta3xlwifi-eng
--- lineage_gta3xlwifi.mk
+++ lineage_gta3xlwifi.mk
@@ -4,4 +4,5 @@ $(call inherit-product, device/samsung/gta3xlwifi/full_gta3xlwifi.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_gta3xlwifi
+
 -include vendor/samsung/gta3xlwifi/BoardConfigVendor.mk
